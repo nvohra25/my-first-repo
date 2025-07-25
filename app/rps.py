0a235e9d03e25f17102bf984518682c05baab361
nvohra25 <nvohra@nyu.edu>
2025-07-24 20:27:56 -0400

    rps

---
+++ app/rps.py
@@ -0,0 +1,16 @@
+# this is my rock paper scissors game
+
+print("Welcome to my game")
+
+player_choice = input("Please select an option ('rock', 'paper', 'scissors'): ")
+
+print("USER CHOSE:", player_choice)
+
+import random
+
+VALID_OPTIONS = ("rock", "paper", "scissors")
+
+computer_choice - random.choice(VALID_OPTIONS)
+print("COMPUTER CHOSE:", computer_choice)
+
+print("WINNER: TODO")
